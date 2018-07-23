3c3f28bf6aa9bfabe55e164f4b6df420ad290ce8
20158711 <1280334378@qq.com>
2018-07-23 15:15:58 +0800

    添加测试

---
+++ src/test/java/SpringTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-application.xml"})
+//@ContextConfiguration(locations = { "classpath:applicationContext.xml","classpath:servlet-context.xml"})
+public class SpringTest {
+
+    @Autowired
+    private User user;
+
+    @Test
+    public void test(){
+        System.out.println(user);
+    }
+
+}
